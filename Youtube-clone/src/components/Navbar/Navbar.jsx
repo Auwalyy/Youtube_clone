618d82587bf299ca23197767202576164a6762da
Auwalu Muhammad Izziddin <117743968+Auwaluncy@users.noreply.github.com>
2024-12-28 21:56:44 +0100

    commit

---
+++ Youtube-clone/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-div'>
+      <div className="nav-left flex-div"></div>
+    </nav>
+  )
+}
+
+export default Navbar
