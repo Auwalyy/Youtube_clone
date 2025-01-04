1da6127101a63a4b33c78f051b592c21df800c8e
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-01-04 22:26:43 +0100

    Add the youtube api

--- Youtube-clone/src/pages/Home/Home.jsx
+++ Youtube-clone/src/pages/Home/Home.jsx
@@ -1,17 +1,21 @@
 import Feed from '../../components/Feed/Feed'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import './Home.css'
+import React, { useState } from 'react'
 
 // eslint-disable-next-line react/prop-types
 const Home = ({sidebar}) => {
+
+  const [category, setCategory] = useState=(0)
+
   return (
     <>
-      <Sidebar sidebar={sidebar}/>
+      <Sidebar sidebar={sidebar} category={category} setCategory={setCategroy} />
       <div className={`container ${sidebar? "" : "large-container"}`}>
-        <Feed />
+        <Feed category={category} />
       </div>
     </>
-  )
+  ) 
 }
 
 export default Home
