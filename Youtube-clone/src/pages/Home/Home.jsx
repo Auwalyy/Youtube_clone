1320c6bef996687d61453602d99229ec68422cb3
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-01-05 21:24:20 +0100

    commit

--- Youtube-clone/src/pages/Home/Home.jsx
+++ Youtube-clone/src/pages/Home/Home.jsx
@@ -1,16 +1,16 @@
 import Feed from '../../components/Feed/Feed'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import './Home.css'
-import React, { useState } from 'react'
+import  { useState } from 'react'
 
 // eslint-disable-next-line react/prop-types
 const Home = ({sidebar}) => {
 
-  const [category, setCategory] = useState=(0)
+  const [category, setCategory] = useState(0)
 
   return (
     <>
-      <Sidebar sidebar={sidebar} category={category} setCategory={setCategroy} />
+      <Sidebar sidebar={sidebar} category={category} setCategory={setCategory} />
       <div className={`container ${sidebar? "" : "large-container"}`}>
         <Feed category={category} />
       </div>
